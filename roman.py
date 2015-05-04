42b9d48b98762f0cdb05af9e61e4ec5aef875a78
Susan E. McGregor <susan.e.mcgregor@gmail.com>
2015-05-03 22:09:32 -0400

    Converted map to lowercase.

--- roman.py
+++ roman.py
@@ -23,19 +23,19 @@ class NotIntegerError(RomanError): pass
 class InvalidRomanNumeralError(RomanError): pass
 
 #Define digit mapping
-romanNumeralMap = (('M',  1000),
-                   ('CM', 900),
-                   ('D',  500),
-                   ('CD', 400),
-                   ('C',  100),
-                   ('XC', 90),
-                   ('L',  50),
-                   ('XL', 40),
-                   ('X',  10),
-                   ('IX', 9),
-                   ('V',  5),
-                   ('IV', 4),
-                   ('I',  1))
+romanNumeralMap = (('m',  1000),
+                   ('cm', 900),
+                   ('d',  500),
+                   ('cd', 400),
+                   ('c',  100),
+                   ('xc', 90),
+                   ('l',  50),
+                   ('xl', 40),
+                   ('x',  10),
+                   ('ix', 9),
+                   ('v',  5),
+                   ('iv', 4),
+                   ('i',  1))
 
 def toRoman(n):
     """convert integer to Roman numeral"""
